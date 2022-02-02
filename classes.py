4c874c24d07fdabaff0c49298eb0cf34b70d7712
aaselivanov <aaselivanov.10.03@gmail.com>
2022-02-03 02:29:17 +0300

    Текущее состояние проекта. Машинки едут по пустой карте, агент управляется вручную, отрисовывается весть путь ведущего.

---
+++ classes.py
@@ -0,0 +1,182 @@
+import pygame
+from math import pi, degrees, radians, cos, sin, atan, acos, asin, sqrt
+import numpy as np
+from scipy.spatial import distance
+from pygame.math import Vector2
+
+class GameObject():
+    def __init__(self,
+                 name,
+                 image=None,
+                 start_position=None,
+                 height=None,
+                 width=None):
+        self.name = name
+        self.image = image
+        
+        if height and width:
+            self.image = pygame.transform.scale(self.image, (width,height))
+            
+        self.start_position = np.array(start_position)
+        self.position = self.start_position
+        
+        
+    def put(self,position):
+        # TODO: collisions
+        self.position = np.array(position)
+    
+#     def show(self):
+        
+        
+
+class AbstractRobot(GameObject):
+    def __init__(self, 
+                 name,
+                 image=None,
+                 start_position=None,
+                 height=None,
+                 width=None,
+                 min_speed=0., 
+                 max_speed=2.,
+                 max_rotation_speed=60, # в градусах
+                 max_speed_change=0.5,
+                 max_rotation_speed_change=57, # в градусах
+                 start_direction = 0, # в градусах
+                 **kwargs
+                 ):
+        # TODO: задание формы робота полигоном или абстрактной фигурой
+        
+        super().__init__(name,
+                 image=image,
+                 start_position=start_position,
+                 height=height,
+                 width=width)
+        
+        self.speed = 0.
+        self.rotation_speed = 0. # в радианах
+        self.rotation_direction = 0
+        # 0 -- прямо, -1 -- влево, +1 -- вправо.
+        
+        self.direction = start_direction # в градусах!
+        
+        self.desirable_rotation_speed = 0. # в радианах
+        self.desirable_rotation_direction = 0.
+        self.desirable_speed = 0.
+        
+        self.min_speed = min_speed
+        self.max_speed = max_speed
+        
+        self.max_rotation_speed = max_rotation_speed
+        
+        self.max_speed_change = max_speed_change
+        self.max_rotation_speed_change = max_rotation_speed_change
+        
+        
+        
+    def command_turn(self, desirable_rotation_speed, rotation_direction):
+        """Обработка команд, влияющих на скорость угловую w"""
+            # Не превышаем максимальной скорости
+        
+        self.desirable_rotation_speed = min(desirable_rotation_speed,self.max_rotation_speed)
+        self.desirable_rotation_direction = rotation_direction
+        
+        if (rotation_direction == 0) and (desirable_rotation_speed!=0):
+            raise ValueError("Указана скорость поворота, но направление = 0!")
+        
+        
+    def command_forward(self, desirable_speed):
+        """Обработка команд, влияющих на скорость v"""
+        if desirable_speed > self.max_speed:
+            desirable_speed = self.max_speed
+        
+        if desirable_speed < self.min_speed:
+            desirable_speed = self.min_speed
+        
+        self.desirable_speed = desirable_speed
+    
+    
+    def _controller_call(self):
+        """Изменение скорости в зависимости от установленных желаемых скоростей на основе управеения"""
+        self._turn_processing()
+        self._speed_processing()
+     
+    
+    def _turn_processing(self):
+        """Обработка изменения скорости поворота в такт для контроллера"""
+        if self.rotation_direction == self.desirable_rotation_direction:
+            needed_change = abs(self.rotation_speed - self.desirable_rotation_speed)
+            speed_rotation_change = min((needed_change, self.max_rotation_speed_change))
+            
+            if self.desirable_rotation_speed < self.rotation_speed:
+                speed_rotation_change = -1*speed_rotation_change
+        else:
+            needed_change = abs(self.desirable_rotation_speed + self.rotation_speed)
+            speed_rotation_change = -min((needed_change, self.max_rotation_speed_change))
+        
+        new_rotation_speed = self.rotation_speed + speed_rotation_change
+        
+        if new_rotation_speed < 0:
+            self.rotation_direction = -1*self.rotation_direction
+        
+        self.rotation_speed = abs(new_rotation_speed)
+        
+            
+    def _speed_processing(self):
+        """Обработка изменения скорости в такт для контроллера"""
+        needed_change = abs(self.speed-self.desirable_speed)
+        speed_change  = min(self.max_speed_change, needed_change)
+        
+        if self.speed > self.desirable_speed:
+            speed_change=-1*speed_change
+        
+        self.speed = self.speed + speed_change
+        
+        
+    def move(self):
+        """Функция, которая перемещает робота с учётом установленных желаемых скоростей."""
+        # скорректировали скорости
+        self._controller_call()
+        if self.rotation_speed!=0:
+            self.direction = self.direction + self.rotation_direction*self.rotation_speed
+            if self.direction > 360:
+                self.direction = self.direction-360
+            if self.direction < 0:
+                self.direction = 360+self.direction
+        
+        movement_vec = np.array((cos(radians(self.direction))*self.speed, sin(radians(self.direction))*self.speed))
+        self.position+=movement_vec
+        
+    
+    def move_to_the_point(self, next_point):
+        
+        next_point_scaled = next_point - self.position # таким образом мы рассчитываем положение точки относительно робота
+        
+        
+        if next_point_scaled[0]>0:
+            desirable_angle = degrees(atan(next_point_scaled[1]/next_point_scaled[0]))
+        elif next_point_scaled[0]<0:
+            desirable_angle = degrees(atan(next_point_scaled[1]/next_point_scaled[0]))+180
+        else:
+            desirable_angle = 0
+            
+        if desirable_angle > 360:
+            desirable_angle-=360
+        
+        if desirable_angle<0:
+            desirable_angle=360+desirable_angle
+        
+        delta_turn = self.direction-desirable_angle
+        
+        if delta_turn > 0.:
+            self.rotation_direction=-1
+        elif delta_turn < 0.:
+            self.rotation_direction=1
+        else:
+            self.rotation_direction=0
+        
+        delta_turn = abs(delta_turn)
+
+        self.command_forward(distance.euclidean(self.position,next_point))
+        self.command_turn(abs(delta_turn), self.rotation_direction)
+        
+        self.move()
---
+++ main.py
@@ -0,0 +1,209 @@
+import pygame
+from math import pi, degrees, radians, cos, sin
+import numpy as np
+from classes import AbstractRobot, GameObject
+from scipy.spatial import distance
+
+class Game():
+    def __init__(self, game_width=1024, 
+                 game_height=768,
+                 caption="Serious Robot Follower Simulation v.-1",
+                 trajectory=None,
+                 leader_pos_epsilon=5,
+                 show_leader_path=True,
+                 show_leader_trajectory=True):
+        
+        self.colours = {
+                            'white':(255,255,255),
+                            'black':(0,0,0),
+                            'gray':(30,30,30),
+                            'blue':(0,0,255),
+                            'red':(255,0,0),
+                            'green':(0,255,0)
+                        }
+        
+        self.DISPLAY_WIDTH=game_width
+        self.DISPLAY_HEIGHT=game_height
+        
+        self.trajectory = self.generate_trajectory()
+        
+        pygame.init()
+        self.gameDisplay = pygame.display.set_mode((self.DISPLAY_WIDTH,self.DISPLAY_HEIGHT))
+        pygame.display.set_caption(caption)
+        self.clock = pygame.time.Clock()
+        
+        if trajectory is None:
+            trajectory=self.generate_trajectory()
+        
+        
+        self.leader_pos_epsilon = leader_pos_epsilon
+        
+        self.show_leader_path = show_leader_path
+        self.show_leader_trajectory = show_leader_trajectory
+        
+        
+    
+    def show_object(self,object_to_show):
+        cur_image = object_to_show.image
+        if hasattr(object_to_show, "direction"):
+            cur_image = pygame.transform.rotate(cur_image, -object_to_show.direction)
+            
+        self.gameDisplay.blit(cur_image, object_to_show.position)
+        
+    
+    def generate_trajectory(self, n=3, min_distance = 50, border = 20):
+        """Генерирует точки на карте, по которым должен пройти ведущий"""
+        
+        trajectory = list()
+        
+        while len(trajectory) < n:
+            new_point = np.array((np.random.randint(border,high=self.DISPLAY_WIDTH-border),
+                                  np.random.randint(border,high=self.DISPLAY_HEIGHT-border)))
+            
+            if len(trajectory)==0:
+                trajectory.append(new_point)
+            else:
+                to_add = True
+                
+                for prev_point in trajectory:
+                    if distance.euclidean(prev_point,new_point) < min_distance:
+                        to_add=False
+                
+                if to_add:
+                    trajectory.append(new_point)            
+            
+        return trajectory
+    
+#     def generate_path(self, robot, trajectory):
+#         """На основе списка точек генерирует путь"""
+        
+#         pass
+    
+    
+    
+    
+    def main_loop(self):
+        leader_img =  pygame.image.load("imgs/car_yellow.png")
+        follower_img = pygame.image.load("imgs/car_poice.png")
+        
+        leader = AbstractRobot("leader",
+                            image=leader_img,
+                            height = 20,
+                            width = 30,
+                            start_position=(self.DISPLAY_WIDTH/2, self.DISPLAY_HEIGHT/2))
+
+        follower = AbstractRobot("follower",
+                              image=follower_img,
+                              height = 20,
+                              width = 30,
+                              start_position=((self.DISPLAY_WIDTH/2)+50, (self.DISPLAY_HEIGHT/2)+50))
+
+
+        
+        self.trajectory.insert(0, ((self.DISPLAY_WIDTH/2), (self.DISPLAY_HEIGHT/2)))
+        
+        cur_target_id = 0
+        
+        done = False
+        
+        prev_leader_position = np.zeros(2)
+#         self.gameDisplay.fill(colours["white"])
+
+        self.game_object_list = list()
+        self.game_object_list.append(leader)
+        self.game_object_list.append(follower)
+        
+        self.leader_factual_trajectory = list()
+        
+        leader_finished = False
+        
+        cur_target_point = self.trajectory[1]
+        
+        while not done:
+            self.gameDisplay.fill(self.colours["white"])
+            # TODO: отдельный обработчик команд, чтобы это не было такой сосиской
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+        #                 follower.rotation_direction=-1
+                        if follower.rotation_direction > 0:
+                            follower.rotation_speed=0
+                            follower.rotation_direction=0
+                        else:
+                            follower.rotation_direction=-1
+                            follower.rotation_speed+=5
+                        follower.command_turn(follower.rotation_speed,-1)
+                        print("agent rotation speed and rotation direction", follower.rotation_speed, follower.rotation_direction)
+                        print("current follower direction: ", follower.direction)
+        
+
+                    if (event.key == pygame.K_RIGHT):
+        #                 follower.rotation_direction = 1
+                        if follower.rotation_direction < 0:
+                            follower.rotation_speed=0
+                            follower.rotation_direction=0
+                        else:
+                            follower.rotation_direction=1
+                            follower.rotation_speed+=5
+                        follower.command_turn(follower.rotation_speed,1)
+                        print("agent rotation speed and rotation direction", follower.rotation_speed, follower.rotation_direction)
+                        print("current follower direction: ", follower.direction)
+
+
+                    if event.key == pygame.K_UP:
+                        follower.command_forward(follower.speed+1)
+                        print("agent speed", follower.speed)
+
+                    if event.key == pygame.K_DOWN:
+                        follower.command_forward(follower.speed-1)
+                        print("agent speed", follower.speed)
+            
+            
+            
+            prev_leader_position = leader.position
+            
+            if distance.euclidean(leader.position, cur_target_point) < self.leader_pos_epsilon:
+                cur_target_id+=1
+                if cur_target_id >= len(self.trajectory):
+                    leader_finished = True
+                else:
+                    cur_target_point = self.trajectory[cur_target_id]
+                
+            if not leader_finished:
+                leader.move_to_the_point(cur_target_point)
+            else:
+                leader.command_forward(0)
+                leader.command_turn(0,0)
+            follower.move()
+                
+            self.leader_factual_trajectory.append(leader.position)
+            
+            if self.show_leader_path:
+                pygame.draw.aalines(self.gameDisplay,self.colours["red"],False,self.trajectory)
+            
+            if self.show_leader_trajectory:
+                for cur_point in self.leader_factual_trajectory[:-1]:
+                    pygame.draw.circle(self.gameDisplay, self.colours["black"], cur_point, 5)
+            
+            for cur_object in self.game_object_list:
+                self.show_object(cur_object)
+
+            pygame.display.update()
+            self.clock.tick(60)
+            pygame.time.wait(200)
+
+            
+            
+            
+
+            
+        
+if __name__=="__main__":
+
+    game = Game()
+    game.main_loop()
+    pygame.quit()
+    quit()
