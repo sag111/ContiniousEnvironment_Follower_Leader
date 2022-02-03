633e6667fda5492145990551e093162855bddc83
aaselivanov <aaselivanov.10.03@gmail.com>
2022-02-03 16:40:17 +0300

    добавлены аргументы командной строки

--- main.py
+++ main.py
@@ -8,42 +8,64 @@ from gym.spaces import Box
 
 from stable_baselines3 import PPO
 from time import sleep
+
+import argparse as ag
         
 if __name__=="__main__":
     
-    manual_handling = False
+    parser = ag.ArgumentParser()
+    
+    parser.add_argument('--manual', 
+                        action='store_true', 
+                        help="Если указано, управление осуществляется стрелочками вручную, иначе на основе поданных моделью действий")
+    
+    parser.add_argument('--n_steps',
+                        type=int,
+                        default=5000,
+                        help="Число шагов, в течение которых работает проверочная симуляция")
+    
+    parser.add_argument('--training_steps',
+                    type=int,
+                    default=10000,
+                    help="Число шагов, в течение которых происходит обучение модели (только для режима управления автоматом)")
+    
+#     parser.add_argument('--n_sim',
+#                         type=int,
+#                         default=1,
+#                         help="Число симуляций")
+    
+    args = parser.parse_args()
+    
+    manual_handling = args.manual #False
     
     if manual_handling:
         
         env = gym.make("Test-Cont-Env-Manual-v0")
+        action = (0,0)
         
-        for _ in range(5000):
+        for _ in range(args.n_steps):
             obs, rewards, dones, info = env.step((0,0)) 
             if dones:
                 break
             env.render()
         env.close()
+        
     else:
-    
         env = gym.make("Test-Cont-Env-Auto-v0")
 
         model = PPO("MlpPolicy", env, verbose=1)
-        model.learn(total_timesteps=10000)
-
+        model.learn(total_timesteps=args.training_steps)
 
         print("Обучение закончено")
         sleep(10)
-        print("начинается жара")
+        print("начинается управление")
 
         obs = env.reset()
-
-        for step in range(2000):
+        for step in range(args.n_steps):
 
             action, _states = model.predict(obs)
-#             print(action)
             obs, rewards, dones, info = env.step(action) 
             if dones:
                 break
             env.render()
-#             sleep(0.1)
         env.close()
