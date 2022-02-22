86211bf4a3b0406e23bc561c00e1ea975c20f90b
gilmoright <artem.dahaka@gmail.com>
2022-02-23 01:54:56 +0300

    Исправил ошибку во враппере с радаром то исторических наблюдаемых точек лидера. (при резете не сбрасывалась история). Привел к PEP8 немного, перенёс операции в misc.

--- utils/misc.py
+++ utils/misc.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def angle_correction(angle):
     if angle >= 360:
         return angle - 360
@@ -6,3 +9,22 @@ def angle_correction(angle):
         return 360 + angle
 
     return angle
+
+
+def rotateVector(vec, angle):
+    """
+    Поворачиваем вектора на угол (в градусах)
+    """
+    theta = np.radians(angle)
+    rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+    return np.dot(rot, vec)
+
+
+def calculateAngle(v, w):
+    """
+
+    :param v:
+    :param w:
+    :return:
+    """
+    return np.arccos(v.dot(w) / (np.linalg.norm(v, axis=1) * np.linalg.norm(w)))
