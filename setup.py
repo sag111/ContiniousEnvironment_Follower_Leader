279119a762d9b6c8caa02f3301c85bdd6f8fc6ea
ElonMax <cappukan@gmail.com>
2023-06-13 14:46:14 +0300

    добавлен вариант установки пакета

--- setup.py
+++ setup.py
@@ -14,3 +14,24 @@ setup(
         'numpy>=1.15.0'
     ]
 )
+
+
+# from setuptools import find_packages
+#
+# setup(
+#     name='continuous_grid_arctic',
+#     version='1.0.0',
+#     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
+#     url='https://github.com/sag111/continuous-grid-arctic/',
+#     description='Environment for "follow the leader" task agent training',
+#     packages=find_packages(
+#         where='continuous_grid_arctic',
+#         include=['continuous_grid_arctic', 'continuous_grid_arctic.utils'],
+#         exclude=[],
+#     ),
+#     include_package_data=True,
+#     install_requires=[
+#         'gym>=0.9.6',
+#         'numpy>=1.15.0'
+#     ]
+# )
